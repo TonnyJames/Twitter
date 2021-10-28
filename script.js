f22b9074f3a4f4d015d08253dce33746d6d5f0dc
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2021-10-28 15:46:33 -0300

    alt 3

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// function func(){
+//     alert("teste de alert")
+// }
+
 function func(){
-    alert("teste de alert")
+
+    let obj1 = Object.values(eu)
+    let obj2 = Object.create(eu)
+    alert (obj1)
 }
+
+const eu = {
+    nome: "Tonny Reis",
+    idade: 32,
+
+    endereco: {
+      rua: "uma rua qualquer",
+      numero: 105,
+      cep: "08471-192"
+    },
+
+    familia: {
+      mae: "Mother",
+      pai:"Father",
+      irmao:"Bro",
+      irma: "Sis"
+    },
+
+    pets: [
+      "dog1",
+      "dog2"
+    ]
+  }
